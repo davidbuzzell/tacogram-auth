33270ad4023778513f0e86d8c66ae1e655522005
David Buzzell <david.buzzell@kellogg.northwestern.edu>
2026-03-18 22:46:12 +0000

    encrypt password and signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,25 @@ class SessionsController < ApplicationController
   end
   
   def create
-    # TODO: authenticate user
-    flash["notice"] = "Nope."
+    @user = User.find_by({ "email" => params["email"] })
+    if @user == nil
+      # No user found with this email address
+      flash["notice"] = "Try again"
+      redirect_to "/login"
+    elsif BCrypt::Password.new(@user["password"]) != params["password"]
+      # Incorrect password
+      flash["notice"] = "Try again"
+      redirect_to "/login"
+    else
+      session["user_id"] = @user["id"]
+      flash["notice"] = "Welcome #{@user['first_name']}!"
+      redirect_to "/posts"
+    end
+  end
+
+  def destroy
+    session["user_id"] = nil
+    flash["notice"] = "User signed out"
     redirect_to "/login"
   end
 end
